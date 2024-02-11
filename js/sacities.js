81d5c67bda3dd6137d26122c32da5cb7fad96c8b
ares27 <40366014+ares27@users.noreply.github.com>
2024-02-11 14:40:16 +0200

    Add files via upload
    Added files via upload ...

---
+++ js/sacities.js
@@ -0,0 +1,27 @@
+var testCities =
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Johannesburg",
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [28.0456, -26.2044]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Pretoria",
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [28.1881, -25.7461]
+            }
+        }
+    ]
+}
+
