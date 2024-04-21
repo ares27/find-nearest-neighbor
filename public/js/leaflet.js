7be20717bff28fcf5dc0eb39ddcd55f1f911ba72
ares27 <jehomasb@gmail.com>
2024-04-21 10:56:04 +0200

    updated

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -11,6 +11,11 @@ const Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/
     attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     ext: 'jpg'
 })
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 let cityPointsMarker = {
     radius: 5,
     fillColor: "#0000FF",
